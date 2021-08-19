6f26c98d20cbe3a02a565b697178e6a8269d8c26
Oever González <software@notengobattery.com>
2021-08-19 16:04:17 -0600

    Implement a Bing service and improve the Google service
    Signed-off-by: Oever González <software@notengobattery.com>

--- app/services/bing_search_service.rb
+++ app/services/bing_search_service.rb
@@ -1,8 +1,8 @@
-class GoogleSearchService
+class BingSearchService
   MAPPING = {
-    extract: :description,
-    headline: :title,
-    url: :link
+    extract: :snippet,
+    headline: :name,
+    url: :url
   }
 
   attr_reader :schema
@@ -19,7 +19,7 @@ class GoogleSearchService
   def schema_flatten(schema)
     # Sorting by the digest of the result will lead to a random yet deterministic
     # sorting, allowing the seamless mixture and truncation of the results.
-    schema["results"].sort_by do |element|
+    schema["webPages"]["value"].sort_by do |element|
       Digest::MD5.hexdigest(Marshal.dump(element))
     end
   end
@@ -27,14 +27,15 @@ class GoogleSearchService
   private
 
   def build_uri(query)
-    uri = URI.parse("https://google-search3.p.rapidapi.com/api/v1/search")
-    uri.path += "/q=#{CGI.escape(query)}"
+    uri = URI.parse("https://bing-web-search1.p.rapidapi.com/search")
+    uri.query = "q=#{CGI.escape(query)}"
     uri
   end
 
   def make_request(uri)
     RestClient.get(uri.to_s,
-      x_rapidapi_host: "google-search3.p.rapidapi.com",
+      x_bingapis_sdk: "true",
+      x_rapidapi_host: "bing-web-search1.p.rapidapi.com",
       x_rapidapi_key: Rails.application.credentials.rapidapi[:key])
   end
 end
