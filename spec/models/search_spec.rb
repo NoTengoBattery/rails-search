2ae1306e3212286b436c42c14ed9441c8a823084
Oever González <software@notengobattery.com>
2021-08-18 22:43:20 -0600

    Add a "Search" model spec
    Signed-off-by: Oever González <software@notengobattery.com>

---
+++ spec/models/search_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+require "shared_rutines"
+
+RSpec.describe Search, type: :model do
+  context "with validations" do
+    subject { FactoryBot.build(:search) }
+
+    it { is_expected.to validate_presence_of(:engine) }
+    it { is_expected.to validate_presence_of(:search) }
+  end
+end
